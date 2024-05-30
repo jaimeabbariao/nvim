4761d126b617c74dd1022dcc336a5ff538cba21b
jaimeabbariao <jabbariao@figma.com>
2024-05-30 16:35:31 -0400

    adding more stuff

--- init.lua
+++ init.lua
@@ -532,6 +532,9 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        javascriptreact = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
       },
     },
   },
