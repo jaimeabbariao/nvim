880481f38fa058c3ac0c470c71f426deac4fc13b
jaimeabbariao <jabbariao@figma.com>
2024-06-05 16:17:58 -0400

    updating css

--- init.lua
+++ init.lua
@@ -384,6 +384,9 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         rust_analyzer = {},
+        cssls = {},
+        cssmodules_ls = {},
+        css_variables = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
