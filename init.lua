d97a4c1da809e31f5e8af54b5d16938adfdf239f
ja153903 <jaimeabbariao@gmail.com>
2024-06-11 00:32:28 -0400

    adding some support for cpp

--- init.lua
+++ init.lua
@@ -380,7 +380,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {
+          filetypes = { 'cpp' },
+        },
         gopls = {},
         pyright = {},
         rust_analyzer = {},
@@ -487,6 +489,7 @@ require('lazy').setup({
         typescript = { { 'prettierd', 'prettier' } },
         javascriptreact = { { 'prettierd', 'prettier' } },
         typescriptreact = { { 'prettierd', 'prettier' } },
+        cpp = { 'clang-format' },
       },
     },
   },
