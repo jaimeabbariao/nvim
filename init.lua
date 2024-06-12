da9a57701f3a6b24992eda09178b795750e2941e
ja153903 <jaimeabbariao@gmail.com>
2024-06-12 16:57:50 -0400

    adding some set up for elixir

--- init.lua
+++ init.lua
@@ -390,6 +390,7 @@ else
           pyright = {},
           rust_analyzer = {},
           cssls = {},
+          elixirls = {},
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
           --
           -- Some languages (like typescript) have entire language plugins that can be useful:
