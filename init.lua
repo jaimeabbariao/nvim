a938e5b1fc4ebcd54eae758860c3d8d34c3aef19
jaimeabbariao <jabbariao@figma.com>
2024-06-12 16:42:49 -0400

    adding cssmodules here

--- init.lua
+++ init.lua
@@ -385,6 +385,7 @@ require('lazy').setup({
         pyright = {},
         rust_analyzer = {},
         cssls = {},
+        cssmodules_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
