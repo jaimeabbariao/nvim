b6c30aca1a90c00b7a7e540b1ccb302f31679f87
jaimeabbariao <jabbariao@figma.com>
2024-06-13 12:57:17 -0400

    updating this shit

--- init.lua
+++ init.lua
@@ -408,7 +408,7 @@ require('lazy').setup({
           settings = {
             Lua = {
               completion = {
-                callSnippet = '',
+                callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               diagnostics = { disable = { 'missing-fields' } },
