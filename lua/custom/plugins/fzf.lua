1d0d2d9fd66e101d91f0841a610fad784be532b3
jaimeabbariao <jabbariao@figma.com>
2024-07-12 11:58:43 -0400

    adding grep related stuff to make life easier

--- lua/custom/plugins/fzf.lua
+++ lua/custom/plugins/fzf.lua
@@ -25,6 +25,45 @@ return {
             theme = 'ansi',
           },
         },
+        grep = {
+          prompt = 'Rg❯ ',
+          input_prompt = 'Grep For❯ ',
+          multiprocess = true, -- run command in a separate process
+          git_icons = true, -- show git icons?
+          file_icons = true, -- show file icons?
+          color_icons = true, -- colorize file|git icons
+          -- executed command priority is 'cmd' (if exists)
+          -- otherwise auto-detect prioritizes `rg` over `grep`
+          -- default options are controlled by 'rg|grep_opts'
+          -- cmd            = "rg --vimgrep",
+          grep_opts = '--binary-files=without-match --line-number --recursive --color=auto --perl-regexp -e',
+          rg_opts = '--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
+          -- Uncomment to use the rg config file `$RIPGREP_CONFIG_PATH`
+          -- RIPGREP_CONFIG_PATH = vim.env.RIPGREP_CONFIG_PATH
+          --
+          -- Set to 'true' to always parse globs in both 'grep' and 'live_grep'
+          -- search strings will be split using the 'glob_separator' and translated
+          -- to '--iglob=' arguments, requires 'rg'
+          -- can still be used when 'false' by calling 'live_grep_glob' directly
+          rg_glob = true, -- default to glob parsing?
+          glob_flag = '--iglob', -- for case sensitive globs use '--glob'
+          glob_separator = '%s%-%-', -- query separator pattern (lua): ' --'
+          -- advanced usage: for custom argument parsing define
+          -- 'rg_glob_fn' to return a pair:
+          --   first returned argument is the new search query
+          --   second returned argument are additional rg flags
+          -- rg_glob_fn = function(query, opts)
+          --   ...
+          --   return new_query, flags
+          -- end,
+          --
+          -- Enable with narrow term width, split results to multiple lines
+          -- NOTE: multiline requires fzf >= v0.53 and is ignored otherwise
+          -- multiline      = 1,      -- Display as: PATH:LINE:COL\nTEXT
+          -- multiline      = 2,      -- Display as: PATH:LINE:COL\nTEXT\n
+          no_header = false, -- hide grep|cwd header?
+          no_header_i = false, -- hide interactive header?
+        },
       }
     end,
     keys = {
